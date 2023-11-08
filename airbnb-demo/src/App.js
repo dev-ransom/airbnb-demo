8260de52713a28c7f0fb220a9cd822e6cab73b35
idungstanley <sundaystanley56@gmail.com>
2023-11-08 09:15:11 +0100

    added files and code for nav bar

---
+++ airbnb-demo/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Navbar from './Navbar'
+export default function App() {
+  return (
+    <div className='w-[550px] rounded-[5px] text-white drop-shadow-md bg-slate-50 '>
+      <Navbar />
+    </div>
+  )
+}
